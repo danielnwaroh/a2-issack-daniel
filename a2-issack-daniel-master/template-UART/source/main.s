459be7a18768c16542108583d482f208599f3d2a
danielnwaroh <daniel.nwaroh@gmail.com>
2018-02-15 00:30:16 +0000

    I added error checks for the first and second
    also changed a couple label names

--- a2-issack-daniel-master/template-UART/source/main.s
+++ a2-issack-daniel-master/template-UART/source/main.s
@@ -17,10 +17,18 @@ errorMessage:		@print out error message, bad command
 			ldr		r0,	=fmt6		@ String pointer
 			mov		r1,	#25			@ String's length
 			bl		WriteStringUART	@ Write the string to the UART
+			
+			b		command
+			
+binError:		@print out error message, bad command
+			ldr		r0,	=fmt4		@ String pointer
+			mov		r1,	#22			@ String's length
+			bl		WriteStringUART	@ Write the string to the UART
+			b		askNum
 
 command:			@Ask for the command to be executed
             ldr     r0, =fmt1    	@ String pointer
-            mov     r1, #25        	@ String's length
+            mov     r1, #26        	@ String's length
             bl      WriteStringUART 	@ Write the string to the UART
 			
 			
@@ -35,14 +43,14 @@ command:			@Ask for the command to be executed
 			
 			@comparing string lengths		
 			cmp 	r0,	#3
-			beq		loopAND
+			beq		andOp
 			cmp		r0,	#2
-			beq		loopOR
+			beq		orOp
 			b		errorMessage
 			
 			@compare input with the string "AND"		
-loopAND:	ldrb	r9,	=andString
-andTop:				
+andOp:		
+			mov		r9,	#1
 			ldrb	r5,	[r10, #0]
 			ldrb	r6,	[r10, #1]
 			ldrb	r7,	[r10, #2]
@@ -56,13 +64,14 @@ andTop:
 			cmp	r7,	#68
 			bne	errorMessage
 
-			b	done
+			b	askNum
 
 			
 
 			@compare input with the string "OR"
-loopOR:		ldrb	r9,	=orString
-orTop:		
+orOp:		
+			mov		r9,	#2
+			
 			ldrb	r5,	[r10, #0]
 			ldrb	r6,	[r10, #1]
 
@@ -72,12 +81,12 @@ orTop:
 			cmp	r6,	#82
 			bne	errorMessage
 
-			b		done
+			b		askNum
 			
-done:		
+askNum:		
 			@Ask for the first input
             ldr     r0, =fmt2    	@ String pointer
-            mov     r1, #39        	@ String's length
+            mov     r1, #40        	@ String's length
             bl      WriteStringUART 	@ Write the string to the UART
 
 
@@ -85,10 +94,13 @@ done:
             mov     r1, #64    		@ buffer size
             bl      ReadLineUART 	@ Read from the UART until a new line is encountered. 
 					@ R0 = number of ASCII characters read.
-					
+			
+			ldr		r4,	=charBuffer
+			bl		binaryCheck
+			
 			@Ask for the second input
             ldr     r0, =fmt3    	@ String pointer
-            mov     r1, #40        	@ String's length
+            mov     r1, #41        	@ String's length
             bl      WriteStringUART 	@ Write the string to the UART
 
 
@@ -97,26 +109,68 @@ done:
             bl      ReadLineUART 	@ Read from the UART until a new line is encountered. 
 					@ R0 = number of ASCII characters read
 					
-
+			ldr		r4,	=charBuffer
+			bl		binaryCheck
+			
 	haltLoop$:  
 			b       haltLoop$
+			
+
+binaryCheck:
+			push	{r0, r4, r9, fp, lr}
+			mov		fp,	sp
+			sub		sp,	#12
+			
+			ldrb	r0,	[r4, #0]
+			ldrb	r1,	[r4, #1]
+			ldrb	r2,	[r4, #2]
+			ldrb	r3,	[r4, #3]
+			
+			cmp		r0,	#48
+			blt		binError
+			
+			cmp		r0,	#57
+			bgt		binError
+			
+			cmp		r1,	#48
+			blt		binError
+			
+			cmp		r1,	#57
+			bgt		binError
+			
+			cmp		r2,	#48
+			blt		binError
+			
+			cmp		r2,	#57
+			bgt		binError
+			
+			cmp		r3,	#48
+			blt		binError
+			
+			cmp		r3,	#57
+			bgt		binError
+			
+			add		sp,	#12
+			pop		{r0, r4, r9, fp, pc}
+			@mov		pc,	lr
+
 
 @ Data section
 .section    .data
 
 @input prints
 fmt1:
-.ascii		"Please enter a command:\r\n"
+.ascii		"Please enter a command:\r\n>"
 fmt2:
-.ascii		"Please enter the first binary number:\r\n"
+.ascii		"Please enter the first binary number:\r\n>"
 fmt3:
-.ascii		"Please enter the second binary number:\r\n"
+.ascii		"Please enter the second binary number:\r\n>"
 
 @prints
 fmt:
 .ascii		"Creator names: Issack John and Daniel Nwaroh\r\n"
 fmt4:
-.ascii		"wrong number format!\r\n"
+.ascii		"Wrong number format!\r\n"
 fmt5:
 .ascii		"The result is: %d\r\n"
 fmt6:
